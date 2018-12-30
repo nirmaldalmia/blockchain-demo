016178fb4b30186f48de2ababdbbae45a4d161a5
nirmaldalmia <nirmaldalmia17@gmail.com>
2018-12-30 17:01:43 +0530

    Added flask components

--- blockchain.py
+++ blockchain.py
@@ -2,6 +2,8 @@ import hashlib
 import json
 from time import time
 from uuid import uuid4
+from textwrap import dedent
+from flask import Flask
 
 
 class Blockchain(object):
@@ -104,3 +106,33 @@ class Blockchain(object):
         guess = f'{last_proof}{proof}'.encode()
         guess_hash = hashlib.sha256(guess).hexdigest()
         return guess_hash[:4] == "0000"
+    
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain)
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
