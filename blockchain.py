3c83c209ea4dbfb13d12e6f6e13db8c1f2193f7b
nirmaldalmia <nirmaldalmia17@gmail.com>
2018-12-30 17:14:04 +0530

    Added method definitions for flask

--- blockchain.py
+++ blockchain.py
@@ -3,7 +3,7 @@ import json
 from time import time
 from uuid import uuid4
 from textwrap import dedent
-from flask import Flask
+from flask import Flask, jsonify, request
 
 
 class Blockchain(object):
@@ -120,11 +120,49 @@ blockchain = Blockchain()
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new block"
+    # Run the proof of work algorithm to get the next proof
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We must receive a reward for finding the proof
+    # The sender is "0" to signify that this node has mined a new coin
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1
+    )
+
+    #Forge the new block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New block Forged!",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+    }
+
+    return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "We'll add a new transaction"
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+    
+    # Create a new transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {
+        'message': f'Transaction will be added to block {index}'
+    }
+    return jsonify(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
